ebf4eb89a1f5d2a90a533f7ed6a104bf61789941
jmarreros@gnbit.com <jmarrerosurquiza@gmail.com>
2025-05-27 10:27:08 -0500

    pasarela

--- src/app/modules/landing/home/section9/section9.component.ts
+++ src/app/modules/landing/home/section9/section9.component.ts
@@ -8,4 +8,27 @@ import { Component } from '@angular/core';
 export class Section9Component {
     yearlyBilling: boolean = true;
 
+    
+  planPeriod: 'mensual' | 'anual' = 'mensual';
+
+  prices = {
+    basico: {
+      mensual: 33,
+      anual: 29, // 20% descuento por anual
+    },
+    pro: {
+      mensual: 57,
+      anual: 49, // sin descuento, se factura 1500 en total
+      anualFacturado: 1500,
+    },
+    enterprise: {
+      mensual: 69,
+      anual: 59, // 15% descuento por anual (ejemplo)
+      anualFacturado: 2550, // podría ser distinto al cálculo para mostrar facturado
+    },
+  };
+
+  setPlanPeriod(period: 'mensual' | 'anual') {
+    this.planPeriod = period;
+  }
 }
