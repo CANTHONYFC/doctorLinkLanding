4a321ffa10f9335456147db2989df85710993ae6
anthony falcon <43174667+CANTHONYFC@users.noreply.github.com>
2025-05-30 16:04:20 -0500

    limpieza

--- src/environments/enviroment.conts.ts
+++ src/environments/enviroment.conts.ts
@@ -1,13 +1,13 @@
 export const HOST = {
     local: 'https://localhost:7189/',
-    test: 'http://localhost:9040/',
-    prod: 'https://localhost:7189/',
+    test: 'https://web-doctorlink-service.itbcpwebservices.com/',
+    prod: 'https://doctor-link-back-673gca465q-uc.a.run.app/',
 
 };
 
 export const hostSelected = {
     host: HOST.local,
-    hostProd: HOST.test,
+    hostProd: HOST.prod,
 };
 
 
