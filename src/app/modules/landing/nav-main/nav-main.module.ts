35783c892db038c346422a30a5fb38fc33b39c98
Cristopher <cristopher_campos6@hotmail.com>
2025-06-19 00:39:30 -0500

    limpieza

---
+++ src/app/modules/landing/nav-main/nav-main.module.ts
@@ -0,0 +1,18 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {NavComponent} from './nav/nav.component';
+import {RouterLink} from "@angular/router";
+
+
+@NgModule({
+    declarations: [
+        NavComponent
+    ],
+    imports: [
+        CommonModule,
+        RouterLink
+    ],
+    exports: [NavComponent]
+})
+export class NavMainModule {
+}
---
+++ src/app/modules/landing/nav-main/nav/nav.component.ts
@@ -0,0 +1,37 @@
+import {Component, HostListener} from '@angular/core';
+
+@Component({
+    selector: 'app-nav-main',
+    templateUrl: './nav.component.html',
+    styleUrls: ['./nav.component.scss']
+})
+export class NavComponent {
+    isMobileMenuOpen = false;
+    isFuncionesOpen = false;
+
+    toggleMenu() {
+        this.isMobileMenuOpen = !this.isMobileMenuOpen;
+    }
+
+    toggleFunciones() {
+        this.isFuncionesOpen = !this.isFuncionesOpen;
+    }
+
+    // Opcional: cerrar al hacer clic fuera del menú
+    @HostListener('document:click', ['$event'])
+    onDocumentClick(event: Event) {
+        const target = event.target as HTMLElement;
+        // Aquí pones el id o clase del menú para comparar
+        if (!target.closest('.funciones-menu')) {
+            this.isFuncionesOpen = false;
+        }
+    }
+
+    isMobileFuncionesOpen = false;
+
+
+    toggleMobileFunciones() {
+        this.isMobileFuncionesOpen = !this.isMobileFuncionesOpen;
+    }
+
+}
